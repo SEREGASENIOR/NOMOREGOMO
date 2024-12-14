4118625d7004be09f2414471d078c45f0bdeb574
SEREGASENIOR <161219928+SEREGASENIOR@users.noreply.github.com>
2024-12-14 15:43:08 +0500

    Add files via upload

---
+++ src/scripts/index1.js
@@ -0,0 +1,3 @@
+ export const profilePopup = document.querySelector('.popup_type_edit');
+ export const cardPopup = document.querySelector('.popup_type_new-card');
+export const imagePopup = document.querySelector('.popup_type_image');
