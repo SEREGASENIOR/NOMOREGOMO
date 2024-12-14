779ccbb4775af39cb16246da42bba07999e5c0b2
SEREGASENIOR <161219928+SEREGASENIOR@users.noreply.github.com>
2024-12-14 13:43:49 +0300

    Delete scripts directory

--- scripts/index.js
+++
@@ -1,3 +0,0 @@
-const profilePopup = document.querySelector('.popup_type_edit');
-const cardPopup = document.querySelector('.popup_type_new-card');
-const imagePopup = document.querySelector('.popup_type_image');
